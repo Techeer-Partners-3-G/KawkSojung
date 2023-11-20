c9c1644d73a8ac78c3e379fcafcfa533a4f4fb8f
ssojungg <kawy.sojung@gmail.com>
2023-11-20 17:54:49 +0900

    Springboot 초기세팅

---
+++ Techeer_Partnerss/src/main/java/com/example/Techeer_Partnerss/TecheerPartnerssApplication.java
@@ -0,0 +1,13 @@
+package com.example.Techeer_Partnerss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheerPartnerssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheerPartnerssApplication.class, args);
+	}
+
+}
